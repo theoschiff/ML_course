2c50a3f8804123f3c3b6a9f91e9a8130cf69ae6c
Theo Schifferli <theo.schifferli@epfl.ch>
2024-10-11 17:41:30 +0200

    Added ex05 implementation

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -9,9 +9,11 @@ import numpy as np
 
 def least_squares(y, tx):
     """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    # normal equation: w = (X^T * X)^(-1) * X^T * y
+    w = np.linalg.solve(tx.T.dot(tx), tx.T @ y) # can put both : either do .dot() or @, equiv
+    
+    # Calculate error and MSE
+    e = y - tx.dot(w)
+    mse = np.mean(e**2) / 2
+    
+    return w, mse
